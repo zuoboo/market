2a1169e34d736420937bb515a2d7bdaac6558e37
github zuoboo <nsnxn1022@gmail.com>
2022-12-09 22:35:41 +0900

    登録画面作成

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome');})->name('top');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
