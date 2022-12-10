13789a022c1c6765a037773202e07b1dd285f107
github zuoboo <nsnxn1022@gmail.com>
2022-12-10 12:23:56 +0900

    eager loading

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 use App\Models\ItemCondition;
 use App\Models\PrimaryCategory;
-
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Http\Request;
 
 class SellController extends Controller
 {
     public function showSellForm() {
-        $categories = PrimaryCategory::orderBy('sort_no')->get();
+        // N+1問題を対策（Eager loading)
+        $categories = PrimaryCategory::query()->with([
+            'secondaryCategories' => function($query) {
+                $query->orderBy('sort_no');
+            }
+        ])->orderBy('sort_no')->get();
+        // $categories = PrimaryCategory::orderBy('sort_no')->get();
         $conditions = ItemCondition::orderBy('sort_no')->get();
 
         return view('sell', compact('conditions', 'categories'));
